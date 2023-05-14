88eb0b99b8a7b0ccb29ecc0df814047fd78cf14e
Junel <junmalaubang@gmail.com>
2023-05-14 16:22:54 +0800

    Added game function for scoring and looping

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const ROCK = 'ROCK',
       PAPER = 'PAPER',
       SCISSORS = 'SCISSORS';
+let score = 0;
 
 // return the computer choice
 function getComputerChoice() {
@@ -17,25 +18,39 @@ function playRound(playerSelection, computerSelection) {
         return "It's a tie!";
     }
     if(playerSelection === ROCK && (computerSelection === PAPER || SCISSORS)) {
-        if (computerSelection === PAPER )
+        if (computerSelection === PAPER ) {
             return loseAlert + 'Paper beats Rock.' ;
-        return winAlert + 'Rock beats Scissors.';
+        }
+            score++;
+            return winAlert + 'Rock beats Scissors.';
     }
     if(playerSelection === PAPER && (computerSelection === ROCK || SCISSORS)) {
-        if (computerSelection === ROCK )
-            return winAlert + 'Paper beats Rock.' ;;        
+        if (computerSelection === ROCK ) {
+            score++;
+            return winAlert + 'Paper beats Rock.' ;
+        }      
         return loseAlert + 'Scissors beat paper';
     }
     if (playerSelection === SCISSORS && (computerSelection === PAPER || ROCK)) {
-        if (computerSelection === PAPER ) 
+        if (computerSelection === PAPER ) {
+            score++;
             return winAlert + 'Scissors beat paper';
+        }
         return loseAlert + 'Rock beats Scissors.';
     }
   }
 
+function game() {
+    let i = 1;
+    while( i <= 5) {
 
-let playerSelection = (prompt('Choose: Paper, Rock and Scissors.', 'rock') || ROCK).toUpperCase();
-let computerSelection = getComputerChoice();
+        let playerSelection = (prompt('Choose: Paper, Rock and Scissors.', 'rock') || ROCK).toUpperCase();
+        let computerSelection = getComputerChoice();
+        console.table({playerSelection: playerSelection, computerSelection: computerSelection })
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Score: ", score);
+        i++;
+    }
+}
 
-console.table({playerSelection: playerSelection, computerSelection: computerSelection })
-console.log(playRound(playerSelection, computerSelection));
+game();
